7fa135f6018ea4b7c1cdb6aabf9ccb08c32ff5b5
Ankit Sharma455 <ankitsharma41655@gmail.com>
2021-02-23 14:04:01 +0530

    dp hashing solved

---
+++ TwoSum.java
@@ -0,0 +1,17 @@
+function twoSum(arr, S) {
+
+  var sums = [];
+  var hashTable = {};
+  for (var i = 0; i < arr.length; i++) {
+    var sumMinusElement = S - arr[i];
+    if (hashTable[sumMinusElement.toString()] !== undefined) { 
+      sums.push([arr[i], sumMinusElement]);
+    }
+    hashTable[arr[i].toString()] = arr[i];
+
+  }
+  return sums;
+
+}
+
+twoSum([3, 5, 2, -4, 8, 11], 7);
